ca5a051bca27f12dfbe7447036ae7628a3582850
mecparts <2958269+mecparts@users.noreply.github.com>
2022-10-17 11:26:15 -0600

    Fixed a minor bug in ATH that's been there since the beginning

--- src/at_basic.h
+++ src/at_basic.h
@@ -290,9 +290,6 @@ char *httpGet(char *atCmd) {
 // ATH go offline (if connected to a host)
 //
 char *hangup(char *atCmd) {
-   if( atCmd[0] ) {
-      ++atCmd;
-   }
    if( tcpIsConnected(tcpClient) ) {
       endCall();
    } else {
